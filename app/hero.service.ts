2b1e9590cd88abe27f81e350eba4af73700e6f8a
Markus Steindl <shomerjs2279@aol.com>
2016-07-12 20:02:34 +0200

    added routings

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -8,4 +8,7 @@ export class HeroService {
         return Promise.resolve(HEROES);
     }
 
+    getHero(id: number) {
+        return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+    }
 }
